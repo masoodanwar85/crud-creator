9d7ef1a8115a5a7c0acc5ffe60631edcdc82f382
Masood Anwar <masoodanwar85@gmail.com>
2026-01-22 20:11:20 +0500

    Updated

--- output/coldfusion_fw1/models/User.cfc
+++
@@ -1,6 +0,0 @@
-component accessors="true" {
-
-property name="name" type="string";
-property name="age" type="string";
-
-}
--- output/sql/migrations/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id int PRIMARY KEY,
-  name varchar(255),
-  email varchar(255),
-  age int,
-  created_at DATETIME,
-  updated_at DATETIME
-);
